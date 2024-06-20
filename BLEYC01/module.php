163258511c9abd4011ac596e1a4e4c01744f4fed
Dennis Ahrndt <blackorca@outlook.de>
2024-06-20 11:35:09 +0200

    first test

--- BLEYC01/module.php
+++ BLEYC01/module.php
@@ -6,14 +6,20 @@ declare(strict_types=1);
 		const MqttParent = "{C6D2AEB3-6E1F-4B2E-8E69-3A1A00246850}";
 		const ModulToMqtt = "{043EA491-0325-4ADD-8FC2-A30C8EEB4D3F}";
 		const MqttToModul = "{7F7632D9-FA40-4F38-8DEA-C83CD4325A32}";
+		const CommandTopic = "cmnd";
+		const RequestCommand = "BLEOp";
+		const ResponseTopic = "tele";
+		const ResultPostfix = "RESULT";
+		const BleResultPostfix = "BLE";
 
 		public function Create()
 		{
 			//Never delete this line!
 			parent::Create();
 
-			$this->RegisterPropertyString("TopicTasmotaDevice", "");
+			$this->RegisterPropertyString("TasmotaDeviceName", "");
 			$this->RegisterPropertyString("MAC", "");
+			$this->RegisterPropertyInteger("RequestInterval", 30);
 
 			$this->ConnectParent(self::MqttParent);
 		}
@@ -30,19 +36,66 @@ declare(strict_types=1);
 			parent::ApplyChanges();
 
 			$this->ConnectParent(self::MqttParent);
+			$this->RegisterTimer('RequestTimer', $this->ReadPropertyInteger('RequestInterval') * 1000 * 60, 'BLEYC01_RequestData($_IPS[\'TARGET\']);');
 
-			$filter = preg_quote('"Topic":"' . $this->ReadPropertyString('TopicTasmotaDevice') . '/' . $this->ReadPropertyString('TopicTasmotaDevice') . '"');
-			$this->SetReceiveDataFilter(".*Hallo.*");
+			$filterResult = preg_quote('"Topic":"' . self::ResponseTopic . '/' . $this->ReadPropertyString('TopicTasmotaDevice') . '/' . self::ResultPostfix . '"');
+			$filterBle = preg_quote('"Topic":"' . self::ResponseTopic . '/' . $this->ReadPropertyString('TopicTasmotaDevice') . '/' . self::BleResultPostfix . '"');
+			
+			$this->SendDebug('Filter ', '.*(' . $filterResult . '|' . $filterBle . ').*', 0);
+        	$this->SetReceiveDataFilter('.*(' . $filterResult . '|' . $filterBle . ').*');
 		}
 
-		public function ReceiveData($JSONString) {
+		public function ReceiveData($JSONString)
+		{
+			if(empty($this->ReadPropertyString('TasmotaDeviceName')) || empty($this->ReadPropertyString('MAC')))
+			{
+				$this->SendDebug("BLEYC01", "TasmotaDeviceName oder MAC Adresse nicht gesetzt", 0);
+				return;
+			}			
 
 			// Empfangene Daten vom Gateway/Splitter
 			$data = json_decode($JSONString);
-			IPS_LogMessage("ReceiveData", utf8_decode($data->Buffer));
+			$data['Payload'] = utf8_decode($data['Payload']);
 			
-			// Sende Resultat zurück an das/den Gateway/Splitter
+			$this->SendDebug('Topic', $data['Topic'], 0);
+            $this->SendDebug('Payload', $data['Payload'], 0);
+
+			$payload = json_decode($data['Payload'], true);
+			$this->SendDebug('Payload decoded', $payload, 0);
+
 			return "OK von " . $this->InstanceID;
+		}
+
+		public function RequestData()
+		{
+			if(empty($this->ReadPropertyString('TasmotaDeviceName')) || empty($this->ReadPropertyString('MAC')))
+			{
+				$this->SendDebug("BLEYC01", "TasmotaDeviceName oder MAC Adresse nicht gesetzt", 0);
+				return;
+			}
+
+			$mac = $this->ReadPropertyString('MAC');
+			if(strlen($mac) == 17)
+			{
+				$mac = str_replace(':', '', $mac);
+			}
+
+			$this->SendDebug('MAC for Payload should be only Hex Values with the length of 12', $mac, 0);
+
+			$topic = self::CommandTopic . '/' . $this->ReadPropertyString('TasmotaDeviceName') . '/' . self::RequestCommand;
+			$payload = "m:" . $this->ReadPropertyString('MAC') . " s:FF01 c:FF02 r go";
+
+			$this->SendDebug('Topic', $topic, 0);
+			$this->SendDebug('Payload', $payload, 0);
+
+			$data['DataID'] = self::ModulToMqtt;
+			$data['PacketType'] = 3;
+			$data['QualityOfService'] = 0;
+			$data['Retain'] = false;
+			$data['Topic'] = $topic;
+			$data['Payload'] = $payload;
+			$dataJSON = json_encode($data, JSON_UNESCAPED_SLASHES);
+			$this->SendDataToParent($dataJSON);
 		}
 	}
 
